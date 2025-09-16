bb3a8a85a2b5d505578261d036325fbc53984d23
muhammad fikri <mfikri.dev@gmail.com>
2025-09-16 17:41:59 +0700

    refactor(index): Remove about section

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,9 +2,7 @@
 // Impor semua section Anda di sini
 import HomeSection from '~/components/home/HomeSection.vue'
 import TechStack from '~/components/home/TechStack.vue';
-import About from '~/components/home/About.vue';
 import ProjectSection from '~/components/home/ProjectSection.vue';
-import Testimonials from '~/components/home/Testimonials.vue';
 import Contact from '~/components/home/Contact.vue';
 </script>
 
@@ -12,7 +10,6 @@ import Contact from '~/components/home/Contact.vue';
   <main>
     <HomeSection />
     <TechStack />
-    <About />
     <ProjectSection />
     <Contact />
   </main>
